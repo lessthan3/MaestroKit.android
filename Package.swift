84ed6cc47d80c257c7c9e0ee19de03931849ed6b
Rok Oblak <oblakr24@gmail.com>
2026-02-11 01:39:51 +0700

    Add root Package.swift for SPM discovery

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "MaestroSDK",
+    platforms: [.iOS(.v14), .tvOS(.v14)],
+    products: [
+        .library(name: "espnKit", targets: ["espnKit"]),
+        .library(name: "foxKit", targets: ["foxKit"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "espnKit",
+            url: "https://github.com/lessthan3/MaestroKit.android/releases/download/4.0.0-SNAPSHOT13.58/espnKit-4.0.0-SNAPSHOT13.58.zip",
+            checksum: "7cc25ec784f05d5cfee301b4c30fbdbfb2a1be8e21e2e4308fe6d33d3fbaa369"
+        ),
+        .binaryTarget(
+            name: "foxKit",
+            url: "https://github.com/lessthan3/MaestroKit.android/releases/download/4.0.0-SNAPSHOT.70/foxKit-4.0.0-SNAPSHOT.70.zip",
+            checksum: "f0b9472d687c4ae2b96d8446baeccbd07c830196bee3ea8c456e357bd5b04a77"
+        ),
+    ]
+)

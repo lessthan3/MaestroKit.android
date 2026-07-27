d223da5c441826c9abec234afc59a534a6500b93
Rok Oblak <32245831+oblakr24@users.noreply.github.com>
2026-07-27 18:43:48 +0200

    Add paramountKit product and binaryTarget

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     products: [
         .library(name: "espnKit", targets: ["espnKit"]),
         .library(name: "foxKit", targets: ["foxKit"]),
+        .library(name: "paramountKit", targets: ["paramountKit"]),
     ],
     targets: [
         .binaryTarget(
@@ -18,6 +19,11 @@ let package = Package(
             name: "foxKit",
             url: "https://github.com/lessthan3/MaestroKit.android/releases/download/foxKit-4.0.24.262/foxKit-4.0.24.262.zip",
             checksum: "8a81901ff6e27a674794dceb61fce916c15d9da7e4dd193a1ffa831cb1ac6958"
+        ),
+        .binaryTarget(
+            name: "paramountKit",
+            url: "https://github.com/lessthan3/MaestroKit.android/releases/download/paramountKit-4.0.24.262/paramountKit-4.0.24.262.zip",
+            checksum: "3b4f14b6782d544e08156719c0d20d848e9aae5ac3ea5f63345c8a50159a411c"
         ),
     ]
 )

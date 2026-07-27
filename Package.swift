4b4277343c383a5725dd3c1a6b1a252088ffafc4
Rok Oblak <32245831+oblakr24@users.noreply.github.com>
2026-07-27 18:51:50 +0200

    Drop unused espnKit binaryTarget

--- Package.swift
+++ Package.swift
@@ -5,16 +5,10 @@ let package = Package(
     name: "MaestroSDK",
     platforms: [.iOS(.v14), .tvOS(.v14)],
     products: [
-        .library(name: "espnKit", targets: ["espnKit"]),
         .library(name: "foxKit", targets: ["foxKit"]),
         .library(name: "paramountKit", targets: ["paramountKit"]),
     ],
     targets: [
-        .binaryTarget(
-            name: "espnKit",
-            url: "https://github.com/lessthan3/MaestroKit.android/releases/download/4.0.0-SNAPSHOT13.58/espnKit-4.0.0-SNAPSHOT13.58.zip",
-            checksum: "7cc25ec784f05d5cfee301b4c30fbdbfb2a1be8e21e2e4308fe6d33d3fbaa369"
-        ),
         .binaryTarget(
             name: "foxKit",
             url: "https://github.com/lessthan3/MaestroKit.android/releases/download/foxKit-4.0.24.262/foxKit-4.0.24.262.zip",
